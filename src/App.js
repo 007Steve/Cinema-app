c2310e8f469b5ddd40eafdc31ebf84c12211b642
007steve <stephen.plummer1.sp@gmail.com>
2020-12-29 22:46:11 -0500

    add user slice

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import React from "react";
-import Header from "./Header.js";
+import React, { useEffect } from "react";
 import "./App.css";
-import Row from "./Row.js";
-import fetch from "./fetch";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import ShowPage from "./Pages/ShowPage";
+import Login from "./Pages/Login";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, logout, login } from "./features/userSlice";
+import { auth } from "./firebase";
 
 function App() {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            picture: authUser.photoURL,
+            email: authUser.email,
+            name: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
   return (
-    <div className="App">
-      <Header />
-      <Row title="Trending" url={fetch.fetchTrending} />
-      <Row title="Action" url={fetch.fetchAction} />
-      <Row title="Comedy" url={fetch.fetchComdey} />
-      <Row title="Top Rated" url={fetch.fetchTopRated} />
-      <Row title="Horror" url={fetch.fetchHorror} />
-      <Row title="Romance" url={fetch.fetchRomance} />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact>
+            {user ? <HomePage /> : <Login />}
+          </Route>
+          <Route path="/movies/:id" exact component={ShowPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
