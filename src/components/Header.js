e3dbd870decc426da728091f633f94f01cc4c4fe
007steve <stephen.plummer1.sp@gmail.com>
2021-01-03 13:46:29 -0500

    removed usestatee

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,13 @@
-import React,{useEffect,useState} from "react";
+import React from "react";
 import "../styles/Header.css";
-import Nav from "./Nav";
 import Banner from "./Banner";
 import fetch from "../fetch";
 
 function Header() {
- 
   return (
-  
-      <div className="header">
-        <Banner url={fetch.fetchTrending} />
-      </div>
-  
+    <div className="header">
+      <Banner url={fetch.fetchTrending} />
+    </div>
   );
 }
 
