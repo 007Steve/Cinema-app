88bb1500bd1fcb377a7bdae63d01e857343ee7dd
007steve <stephen.plummer1.sp@gmail.com>
2021-01-21 15:23:53 -0500

    add nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,13 @@ import React from "react";
 import "../styles/Header.css";
 import Banner from "./Banner";
 import fetch from "../fetch";
-
+import Nav from "./Nav";
 function Header() {
   return (
-    <div className="header">
+    <div className="header ">
+      <Nav />
       <Banner url={fetch.fetchTrending} />
+      <h1 className="header__texts">fsfhhs</h1>
     </div>
   );
 }
