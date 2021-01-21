cd13154baff8cc58c66c7ade4123d1ddf0998a62
007steve <stephen.plummer1.sp@gmail.com>
2021-01-21 15:21:52 -0500

    removed banner

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -3,11 +3,12 @@ import "../styles/HomePage.css";
 import Row from "../components/Row.js";
 import fetch from "../fetch";
 import Banner from "../components/Banner";
+import Header from "../components/Header";
 
 function HomePage() {
   return (
     <div className="homePage">
-      <Banner url={fetch.fetchTrending} />
+     <Header/>
       <Row title="Trending" url={fetch.fetchTrending} />
       <Row title="Action" url={fetch.fetchAction} />
       <Row title="Comedy" url={fetch.fetchComdey} />
